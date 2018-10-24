cdeddc530cd18add1e1c6ef86b77629a1b2a940d
Marc-Frederick <mtrem122@uottawa.ca>
2018-10-24 19:45:20 -0400

    ChinaWhite

--- app/src/main/java/collectifyamoukoudji/projetseg/MainActivity.java
+++ app/src/main/java/collectifyamoukoudji/projetseg/MainActivity.java
@@ -1,27 +1,28 @@
 package collectifyamoukoudji.projetseg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.*;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button button1 = (login)findViewById(R.id.button);
-    button.setOnClickListener(new View.OnCliclListener()
-
+    button.setOnClickListener(new View.OnClickListener()
     {
-        public void login (View view){
-        Intent intent = new Intent(this, LogIn.class);
-        startActivity(intent);
-    }
-    }
+        public void onClick(View v){
+            Intent intent = new Intent(this, LogIn.class);
+            startActivity(intent);
+        }
+    });
     Button button2 = (sign_up)findViewById(R.id.button);
-    button2.setOnClickListener(new View.OnCliclListener()
-
+    button2.setOnClickListener(new View.OnClickListener()
     {
-        public void sign_up (View view){
-        Intent intent = new Intent(this, LogIn.class);
-        startActivity(intent);
-    }
-    }
+        public void onClick(View v){
+            Intent intent = new Intent(this, LogIn.class);
+            startActivity(intent);
+        }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
