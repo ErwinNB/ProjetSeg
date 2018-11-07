902186118fadf8ddf9668af3afa1eeb70e68a886
Akabo027 <unkabore@gmail.com>
2018-11-07 18:11:11 -0500

    Test sur les Users --> OK

--- app/src/test/java/collectifyamoukoudji/projetseg/ServiceTest.java
+++ app/src/test/java/collectifyamoukoudji/projetseg/ServiceTest.java
@@ -5,16 +5,18 @@ import org.junit.Test;
 
 public class ServiceTest {
 
+    Service service = new Service("Toilette", 200);
+
     @Test
     public void checkSerciceName(){
-        Service service = new Service("Toilette", 200);
         assertEquals("check the name of the service", "Toilette", service.getServiceName());
+        System.out.println("Service name --> OK");
     }
 
     @Test
     public void checkServiceRate(){
-        Service service = new Service("Toilette", 200.0);
         assertEquals("check the rate of the service", "200.0", String.valueOf(service.getRate()));
+        System.out.println("Service rate --> OK");
     }
 
 }
