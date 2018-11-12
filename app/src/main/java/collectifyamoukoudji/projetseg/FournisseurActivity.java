c0d2dc82d7b3a9819204412337f423f7a3ccbe2c
valentinmagot <valentin_magot@outlook.com>
2018-11-12 14:10:30 -0500

    addef login without id

---
+++ app/src/main/java/collectifyamoukoudji/projetseg/FournisseurActivity.java
@@ -0,0 +1,13 @@
+package collectifyamoukoudji.projetseg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FournisseurActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fournisseur);
+    }
+}
